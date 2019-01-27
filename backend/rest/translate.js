fd0d42eff72460071280c609ef06efe564338986
t.ferreria@live.ca <t.ferreira@live.ca>
2019-01-26 19:32:59 -0500

    shaja

--- backend/rest/translate.js
+++ backend/rest/translate.js
@@ -1,89 +1,13 @@
-var bodyParser =    require("body-parser");
-var multer  =   require('multer');
-var morgan  =   require('morgan');
 
-const express = require("express");
-var router = express.Router();
-
-// // Imports the Google Cloud client library
-// const vision = require('@google-cloud/vision');
-
-// // Creates a client
-// const client = new vision.ImageAnnotatorClient();
-
-// // Performs label detection on the image file
-// client.labelDetection('./resources/wakeupcat.jpg').then(results => {
-//     const labels = results[0].textAnnotations[0].description;
-
-//     console.log('Labels:');
-//     labels.forEach(label => console.log(label.description));
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
-
-
-const storage = multer.diskStorage({
-  destination: 'some-destination',
-  filename: function (req, file, callback) {
-    //..
-  }
-});
-
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
-router.use(morgan('dev'));
-
-router.post('/translate', upload.single('avatar'), (req, res) => {
-
-	res.type("html");
-    res.send("hello");
-
-  if (!req.file) {
-    console.log("No file received");
-    return res.send({
-      success: false
-    });
-
-  } else {
-    console.log('file received');
-    return res.send({
-      success: true
-    })
-  }
-});
-
-const host = req.host;
-const filePath = req.protocol + "://" + host + '/' + req.file.path;
-
-router.use(express.static(__dirname, 'public'));
-
-
-
-
+var express = require('express')
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
-
-// Performs label detection on the image file
-client.labelDetection(filepath).then(results => {
-    var label = results[0].textAnnotations[0].description;
-    this.labels = "";
-    var alpha = "abcdefghijklmnopqrstuvwxyzQAZWSXEDCRFVTGBYHNUJMKLOP";
-    for (var i = 0; i < label.length; i++) {
-    	if (alpha.search(label[i]) != -1  || label[i] === " ") {
-    		this.labels += label[i];
-    	}
-    }
-})
-
-console.log(this.labels);
-
-
 
 const {Translate} = require('@google-cloud/translate');
 
@@ -95,23 +19,31 @@ const translate = new Translate({
   projectId: projectId,
 });
 
-// The text to translate
-const text = this.labels;
-// The target language
-const target = 'en';
 
-// Translates some text into english
-translate
-  .translate(text, target)
-  .then(results => {
+var app = express()
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+	// Performs label detection on the image file
+	client.labelDetection(req.file).then(results => {
+ 	   var label = results[0].textAnnotations[0].description;
+ 	   var labels = "";
+ 	   var alpha = "abcdefghijklmnopqrstuvwxyzQAZWSXEDCRFVTGBYHNUJMKLOP";
+ 	   for (var i = 0; i < label.length; i++) {
+ 			if (alpha.search(label[i]) != -1  || label[i] === " ") {
+    			labels += label[i];
+    		}
+    	}
+    	translateRun(labels)
+    	.then(function(translatedText) {
+    		res.send(translatedText)
+    	})
+    })
+})
+
+async function translateRun(text) {
+	var results = await translate.translate(text, 'en')
     const translation = results[0];
-
     console.log(`Text: ${text}`);
     console.log(`Translation: ${translation}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-
-
-module.exports = router;
+  return results
+}
