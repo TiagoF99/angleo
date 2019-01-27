6c5ff3aee3ba8956f446b556610f2d3122f97e80
Alvin Tang <alvin.tang@mail.utoronto.ca>
2019-01-27 09:44:06 -0500

    Index changes.

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.get('/get/nearby/:latitude/:longitude/:km', function(req, res) {
 });
 
 // GET: Searches locations by query
-app.post('/get/search/query/name/:name', function(req, res) {
+app.get('/get/search/query/name/:name', function(req, res) {
     res.type("json");
     mongo.nameSearch(
         req.params.name,
@@ -49,6 +49,44 @@ app.get('/get/search/exact/id/:id', function(req, res) {
         res
     );
 });
+
+// GOOGLE VISION -> TRANSLATE API
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+const {Translate} = require('@google-cloud/translate');
+var upload = multer({ dest: './media/translate' })
+
+// Instantiates a client
+const translate = new Translate();
+const target = "fr";
+
+app.post('/post/translate', upload.single("image"), function (req, res, next) {
+    client.textDetection(req.file.path).then(results => {
+ 	   var label = results[0].textAnnotations[0].description;
+ 	   var labels = "";
+ 	   var alpha = "abcdefghijklmnopqrstuvwxyzQAZWSXEDCRFVTGBYHNUJMKLOP";
+ 	   for (var i = 0; i < label.length; i++) {
+ 			if (alpha.search(label[i]) != -1  || label[i] === " ") {
+    			labels += label[i];
+    		}
+    	}
+    	translateRun(labels)
+    	.then(function(translatedText) {
+    		res.send(translatedText)
+    	})
+    })
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
+});
+
+async function translateRun(text) {
+	var results = await translate.translate(text, 'en')
+    const translation = results[0];
+  return results
+}
 
 app.listen(app.get('port'), function(){
     console.log( 'Server running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
